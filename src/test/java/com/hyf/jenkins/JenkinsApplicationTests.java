0c5be9e7b65e9789d2d878829f667bb954847864
hyfsy <1577975140@qq.com>
2021-02-07 20:15:43 +0800

    初始化jenkins测试项目

---
+++ src/test/java/com/hyf/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hyf.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
