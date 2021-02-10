2be91f9ee65a5874c3b13bfdf283a566f2484cdf
hyfsy <1577975140@qq.com>
2021-02-10 12:34:13 +0800

    测试构建添加接口访问

--- src/main/java/com/hyf/jenkins/JenkinsApplication.java
+++ src/main/java/com/hyf/jenkins/JenkinsApplication.java
@@ -2,6 +2,7 @@ package com.hyf.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
@@ -15,10 +16,15 @@ public class JenkinsApplication
         SpringApplication.run(JenkinsApplication.class, args);
     }
 
-    @RequestMapping("/hello")
-    public String hello() {
-        System.out.println("hello");
-        return "hello";
+    @Controller
+    static class HelloController
+    {
+
+        @RequestMapping("/hello")
+        public String hello() {
+            System.out.println("hello");
+            return "hello";
+        }
     }
 
 }
