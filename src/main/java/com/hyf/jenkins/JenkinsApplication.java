4c86c2acc9d60d6b684cc4f9d36415b83155d115
hyfsy <1577975140@qq.com>
2021-02-10 12:23:11 +0800

    测试更新构建

--- src/main/java/com/hyf/jenkins/JenkinsApplication.java
+++ src/main/java/com/hyf/jenkins/JenkinsApplication.java
@@ -9,6 +9,8 @@ public class JenkinsApplication
 
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
         SpringApplication.run(JenkinsApplication.class, args);
     }
 
